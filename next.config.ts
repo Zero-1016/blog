42e81361789e6a206e3fd34edb54ac7b4476cc04
Zero-1016 <iolpd42@gmail.com>
2025-04-15 01:03:10 +0900

    chore: prettier setting

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from "next";
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+import type { NextConfig } from 'next'
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 
 const withVanillaExtract = createVanillaExtractPlugin({
-  identifiers: ({ hash }) => `prefix_${hash}`,
-});
+  identifiers: ({ hash }) => `prefix_${hash}`
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default withVanillaExtract(nextConfig);
+export default withVanillaExtract(nextConfig)
