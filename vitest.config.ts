eb9ef202cdb031f721aec9ed13811414150beaf2
Zero-1016 <iolpd42@gmail.com>
2025-04-16 02:20:53 +0900

    fix: test setting

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
-    include: ['**/*.test.{ts,tsx}'],
+    include: ['**/*.spec.{ts,tsx}'],
     exclude: ['node_modules', '.storybook/**/*', '**/*.stories.{ts,tsx}'],
     coverage: {
       provider: 'v8',
