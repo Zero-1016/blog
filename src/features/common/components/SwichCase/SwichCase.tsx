87d419e0beff621e109842b81c154ccadb32ef79
Zero-1016 <iolpd42@gmail.com>
2025-04-24 02:08:11 +0900

    refactor: swichcase type 변경

--- src/features/common/components/SwichCase/SwichCase.tsx
+++ src/features/common/components/SwichCase/SwichCase.tsx
@@ -1,9 +1,15 @@
-export type SwitchCaseProps = {
-  value: string
+type SwitchCaseValue = string | number | symbol | boolean
+
+export type SwitchCaseProps<T extends SwitchCaseValue> = {
+  value: T
   cases: Record<string, React.ReactNode>
   defaultComponent?: React.ReactNode
 }
 
-export function SwitchCase({ value, cases, defaultComponent = null }: SwitchCaseProps) {
-  return cases[value] || defaultComponent
+export function SwitchCase<T extends SwitchCaseValue>({
+  value,
+  cases,
+  defaultComponent = null
+}: SwitchCaseProps<T>) {
+  return cases[String(value)] ?? defaultComponent
 }
