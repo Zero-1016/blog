53282f1a6e1a61f7def40c6ff56af485a6d72156
Zero-1016 <iolpd42@gmail.com>
2025-04-28 00:22:06 +0900

    chore: vitest내 playwright 검사 제거

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
     include: ['**/*.spec.{ts,tsx}'],
-    exclude: ['node_modules', '.storybook/**/*', '**/*.stories.{ts,tsx}'],
+    exclude: ['node_modules', '.storybook/**/*', '**/*.stories.{ts,tsx}', 'src/test/e2e/**/*'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
