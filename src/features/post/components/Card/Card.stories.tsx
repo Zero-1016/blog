f54fe4134c21d903a0eca89afc564f86518adc8f
Zero-1016 <iolpd42@gmail.com>
2025-05-05 21:45:55 +0900

    fix: storybook args 변경

--- src/features/post/components/Card/Card.stories.tsx
+++ src/features/post/components/Card/Card.stories.tsx
@@ -6,24 +6,7 @@ const meta = {
   parameters: {
     layout: 'centered'
   },
-  tags: ['autodocs'],
-  argTypes: {
-    title: {
-      control: 'text'
-    },
-    description: {
-      control: 'text'
-    },
-    image: {
-      control: 'text'
-    },
-    createdAt: {
-      control: 'date'
-    },
-    tags: {
-      control: 'object'
-    }
-  }
+  tags: ['autodocs']
 } satisfies Meta<typeof Card>
 
 export default meta
@@ -31,12 +14,17 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    title: 'Next.js와 TypeScript로 더 나은 웹 개발하기',
-    description:
-      'Next.js와 TypeScript로 더 나은 웹 개발하기, 오늘 배운 것 리액트 18의 새로운 기능과 성능 최적화 전략',
-    image: '/assets/post/react.png',
-    createdAt: new Date().toISOString(),
-    tags: ['Next.js', 'TypeScript', 'React', 'Web Development']
+    fileName: 'test1.mdx',
+    frontmatter: {
+      title: 'Next.js와 TypeScript로 더 나은 웹 개발하기',
+      description:
+        'Next.js와 TypeScript로 더 나은 웹 개발하기, 오늘 배운 것 리액트 18의 새로운 기능과 성능 최적화 전략',
+      image: '/assets/post/react.png',
+      date: '2021-01-01',
+      tags: ['Next.js', 'TypeScript', 'React', 'Web Development']
+    },
+    createdAt: '2021-01-01',
+    modifiedAt: '2021-01-01'
   },
   render: (args) => <Card {...args} />
 }
