c07b8346abf6de8beb1f64cccdaf295937d339a5
Zero-1016 <iolpd42@gmail.com>
2025-05-06 22:20:51 +0900

    feat: post title 추가

--- src/features/post/Container.tsx
+++ src/features/post/Container.tsx
@@ -1,14 +1,23 @@
+import { Txt } from '@/common/components/Typography'
 import { type Post } from '../post/types/post'
 import { FilterCardList } from './components/FilterCardList'
 import { PostFilter } from './components/PostFilter'
 import * as style from './styles.css'
+import { Flex } from '@/utils/Flex'
 
 export const Container = ({ posts }: { posts: Post[] }) => {
   const tags = new Set(posts.flatMap((post) => post.frontmatter.tags))
   return (
     <div className={style.container}>
-      <PostFilter tags={Array.from(tags)} />
-      <FilterCardList posts={posts} />
+      <Flex
+        direction='column'
+        align='center'
+        justify='center'
+        gap={20}>
+        <Txt fontSize='h2'>기억과 경험을 남기는 공간</Txt>
+        <PostFilter tags={Array.from(tags)} />
+        <FilterCardList posts={posts} />
+      </Flex>
     </div>
   )
 }
--- src/features/post/components/FilterCardList/style.css.ts
+++ src/features/post/components/FilterCardList/style.css.ts
@@ -2,7 +2,6 @@ import { vars } from '@/styles/theme.css'
 import { style } from '@vanilla-extract/css'
 
 export const container = style({
-  paddingTop: vars.space.xlarge,
   width: '100%',
   maxWidth: vars.width.section,
   margin: '0 auto'
