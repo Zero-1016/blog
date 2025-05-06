f455d69613483c505e4b704e6af60a8d1cacc775
Zero-1016 <iolpd42@gmail.com>
2025-05-06 14:00:53 +0900

    feat: footer add

--- src/features/home/styles.css.ts
+++ src/features/home/styles.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css'
 
 export const container = style({
   paddingTop: vars.height.header,
+  paddingBottom: vars.height.footer,
   margin: `${vars.padding.root} auto 0`,
   maxWidth: vars.width.container
 })
