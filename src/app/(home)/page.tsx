e9c92b89daf5998eabb2f09503d70fdcb663b996
Zero-1016 <iolpd42@gmail.com>
2025-05-06 13:47:30 +0900

    chore: 메인 화면에서 포스트를 2개만 가져올 수 있도록 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,6 @@ import { HomeContainer } from '@/features/home'
 import { getPosts } from '@/features/post/utils/getPosts'
 
 export default async function Home() {
-  const posts = await getPosts()
-  return <HomeContainer posts={posts} />
+  const recentPosts = (await getPosts()).slice(0, 2)
+  return <HomeContainer posts={recentPosts} />
 }
